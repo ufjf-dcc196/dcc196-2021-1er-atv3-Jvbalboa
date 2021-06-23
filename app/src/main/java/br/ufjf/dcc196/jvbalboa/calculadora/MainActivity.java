0a858de25c202956baabd5b4b4fd9cf72e376846
Joao Balboa <jbalboa@ice.ufjf.br>
2021-06-22 21:03:56 -0300

    Configura layout inicial

--- app/src/main/java/br/ufjf/dcc196/jvbalboa/calculadora/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/jvbalboa/calculadora/MainActivity.java
@@ -1,7 +1,6 @@
 package br.ufjf.dcc196.jvbalboa.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
